65019f9d670b0fd4d07effdc0a77c8d249e2277a
Justin Tormey <justin@blockchain.com>
2017-11-07 13:45:03 -0500

    test(format): test that options files are json formatted

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,12 +1,21 @@
+const fs = require('fs')
+const path = require('path')
 const { validate } = require('../src')
 
 let runTestFor = (env) => {
   describe(env, () => {
-    let options = require(`../${env}/wallet-options.json`)
+    let optionsPath = path.resolve(__dirname, '..', env, 'wallet-options.json')
+    let options = fs.readFileSync(optionsPath).toString()
+
     it('should be valid', () => {
-      let result = validate(options)
+      let result = validate(JSON.parse(options))
       let errors = result.errors.map(e => e.stack)
       expect(errors).toEqual([])
+    })
+
+    it('should be formatted properly', () => {
+      let formatted = JSON.stringify(JSON.parse(options), null, 2)
+      expect(formatted).toEqual(options)
     })
   })
 }
