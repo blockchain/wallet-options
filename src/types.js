f84f11552107a9ba41cac47688397780b7732ec7
schnogz <andrew@blockchain.com>
2019-04-29 15:56:52 -0400

    v4 wallet options schema part 1

--- src/types.js
+++ src/types.js
@@ -45,3 +45,43 @@ exports.webServiceAlert = () => exports.object({
     link: exports.optional(exports.string())
   }))
 })
+
+exports.coin = () => exports.object({
+  airdrop: exports.optional(exports.object({
+    link: exports.optional(exports.string()),
+    image: exports.optional(exports.string())
+  })),
+  availability: {
+    send: exports.bool(),
+    request: exports.bool(),
+    lockbox: exports.bool(),
+    exchangeTo: exports.bool(),
+    exchangeFrom: exports.bool()
+  },
+  campaign: exports.optional(exports.string()),
+  coinCode: exports.string(),
+  coinTicker: exports.string(),
+  colorCode: exports.string(),
+  config: exports.optional(exports.object({
+    network: exports.optional(exports.enumOf(['bitcoin', 'public', 1])),
+    fees: exports.optional(exports.object({
+      regular: exports.optional(exports.number()),
+      priority: exports.optional(exports.number())
+    })),
+    sendTimeOutSeconds: exports.optional(exports.number())
+  })),
+  contractAddress: exports.optional(exports.string()),
+  displayName: exports.string(),
+  hasLockboxSupport: exports.bool(),
+  icons: {
+    default: exports.string(),
+    circle: exports.string(),
+    circleFilled: exports.string()
+  },
+  lastTxFuse: exports.optional(exports.enumOf([86400, 600])),
+  learnMoreLink: exports.optional(exports.string()),
+  minConfirmations: exports.number(),
+  showNewTagSidenav: exports.optional(exports.bool()),
+  txExplorerBaseUrl: exports.string(),
+  txListAppRoute: exports.optional(exports.string())
+})
