60e016ab109f26470c1be3b6555d8ccbe99bd33d
schnogz <andrew@blockchain.com>
2019-06-11 12:18:28 -0400

    undo auto formatting

--- src/types.js
+++ src/types.js
@@ -1,20 +1,36 @@
 const { countries, states, supportedCoins } = require('./data')
 
-exports.arrayOf = (items) => ({ type: 'array', items })
 exports.bool = () => ({ type: 'boolean', required: true })
+
+exports.coin = () => exports.enumOf(supportedCoins)
+
 exports.country = () => exports.enumOf(countries)
-exports.either = (a, b) => ({ oneOf: [a, b] })
-exports.enumOf = (options) => ({ enum: options })
-exports.fraction = () => Object.assign(exports.number(), { minimum: 0, maximum: 1 })
-exports.integer = () => Object.assign(exports.number(), { divisibleBy: 1 })
-exports.just = (val) => exports.enumOf([val])
-exports.localizedMessage = () => ({ type: 'object', required: true, properties: { 'en': exports.string() } })
-exports.nullable = (type) => exports.either(type, { type: 'null' })
-exports.number = () => ({ type: 'number', required: true })
+
 exports.object = (properties) => ({ type: 'object', required: true, properties, additionalProperties: false })
-exports.optional = (type) => Object.assign(type, { required: false })
-exports.state = () => exports.enumOf(states)
+
 exports.string = () => ({ type: 'string', required: true })
+
+exports.number = () => ({ type: 'number', required: true })
+
+exports.either = (a, b) => ({ oneOf: [a, b] })
+
+exports.nullable = (type) => exports.either(type, { type: 'null' })
+
+exports.optional = (type) => Object.assign(type, { required: false })
+
+exports.arrayOf = (items) => ({ type: 'array', items })
+
+exports.enumOf = (options) => ({ enum: options })
+
+exports.just = (val) => exports.enumOf([val])
+
+exports.state = () => exports.enumOf(states)
+
+exports.integer = () => Object.assign(exports.number(), { divisibleBy: 1 })
+
+exports.fraction = () => Object.assign(exports.number(), { minimum: 0, maximum: 1 })
+
+exports.localizedMessage = () => ({ type: 'object', required: true, properties: { 'en': exports.string() } })
 
 exports.webServiceAlert = () => exports.object({
   action: exports.optional(exports.object({
