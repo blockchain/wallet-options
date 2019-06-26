5b2c15bdadc871d6feed41793cbf084498e3e0da
plondon <philip@blockchain.com>
2019-06-26 13:56:12 +0100

    Add sift apiKey to WO envs

--- src/schema.js
+++ src/schema.js
@@ -192,6 +192,9 @@ const v4 = object({
         config: object({
           apiKey: string()
         })
+      }),
+      sift: object({
+        apiKey: string()
       })
     }),
     ios: object({}),
